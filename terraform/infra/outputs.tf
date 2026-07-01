ae734fafd5d51d53d4c9e5e5d963de24eb16167d
Manoj M <manoj.mm7496@gmail.com>
2026-07-01 16:07:04 +0530

    created infra pipelines

--- terraform/infra/outputs.tf
+++ terraform/infra/outputs.tf
@@ -13,3 +13,14 @@ output "eks_node_group_name" {
 output "eks_node_group_arn" {
   value = module.eks.node_group_arn
 }
+
+# ECR outputs
+
+output "backend_repository_url" {
+  value = module.ecr.backend_repository_url
+}
+
+output "frontend_repository_url" {
+  value = module.ecr.frontend_repository_url
+}
+
