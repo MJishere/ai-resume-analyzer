ad23175017c58d69c9dfe7daff7f78d5570c6ea7
Manoj M <manoj.mm7496@gmail.com>
2026-07-12 12:03:58 +0530

    tf update for iam oidc provder

--- terraform/infra/modules/eks/iam.tf
+++ terraform/infra/modules/eks/iam.tf
@@ -1,3 +1,5 @@
+# IAM oidc provider
+
 data "tls_certificate" "eks" {
   url = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
@@ -12,4 +14,44 @@ resource "aws_iam_openid_connect_provider" "eks" {
   thumbprint_list = [
     data.tls_certificate.eks.certificates[0].sha1_fingerprint
   ]
+}
+
+# Iam policy
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  name = "${var.project_name}-aws-load-balancer-controller-policy"
+  description = "Iam policy for aws load balancer controller"
+  
+  policy = file("${path.module}/policies/aws-load-balancer-controller-policy.json")
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  name = "${var.project_name}-aws-load-balancer-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
 }
