8eb5848f31d638f4d7b5ede83be241442293da1b
Manoj M <manoj.mm7496@gmail.com>
2026-07-14 11:28:01 +0530

    updatd eks version to 1.36 from 1.33

--- terraform/infra/modules/eks/main.tf
+++ terraform/infra/modules/eks/main.tf
@@ -4,7 +4,7 @@ resource "aws_eks_cluster" "this" {
   name     = "${var.project_name}-eks-cluster"
   role_arn = var.eks_cluster_role_arn
 
-  version = "1.33"
+  version = "1.36"
 
   vpc_config {
     subnet_ids              = var.private_subnet_ids
